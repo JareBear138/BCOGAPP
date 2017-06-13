c9b674e8418aaddc471fcc11fc65ba7d2216ab4c
Jared Brown <brownjt01@sunyjcc.edu>
2017-06-13 12:19:55 -0400

    Added function docs on homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -26,6 +26,18 @@ export class HomePage {
   constructor(private http: Http,  private nav: NavController, public loading: LoadingController) {
 
   }
+
+
+  /**
+   * The ionViewDidLoad() function executes every time the page is loaded. This
+   * function does not, however, run every time the view is entered. This allows
+   * the page to be cached and prevents an api call if the user uses the back-button
+   * to navigate back to the front page from a linked article
+   *
+   * @params: none
+   *
+   * @returns: none
+   */
   ionViewDidLoad() {
     this.page = '1';
     let loader = this.loading.create({content: 'Loading Front Page Posts...',});
@@ -43,8 +55,35 @@ export class HomePage {
         //this.items = data, loader.dismiss()
 
   }
+
+  /**
+   * The itemTapped() function handles the tap event for the ion-cards on the
+   * front page. When an item is tapped, the view is pushed to the PostDetailPage
+   * and the data within the item object follows along, allowing the post details
+   * to be displayed on the new page.
+   *
+   * @param event
+   *
+   * @param item
+   */
+  itemTapped(event, item) {
+    this.nav.push(PostDetailPage, {
+      item: item
+    });
+  }
+
+  /**
+   * The refreshLoad() function is similar to the ionViewDidLoad() function
+   * but it excludes the ionic loader api as the this function is used in the
+   * pull to refresh handler ( doRefresh() ) which includes its own loading
+   * spinner making the ionic loader api interface redundant
+   *
+   * @param refresher
+   *
+   * @returns none
+   */
   refreshLoad(refresher){
-    this.page = '1';
+    //this.page = '1';
       let p = this.http.get(this.urlPinned).map(res => res.json());
       let i = this.http.get(this.url).map(res => res.json());
       Observable.forkJoin([p, i]).subscribe(results => {
@@ -57,6 +96,17 @@ export class HomePage {
 
 
   }
+
+  /**
+   * The doRefresh() function is the main refresh handler. refreshLoader() is
+   * called and the page updates. Notice that the refresher.complete() method
+   * is located in the refreshLoad() function, subsequently the refresher object
+   * must be passed into the refreshLoad() function.
+   *
+   * @params refresher
+   *
+   * @returns none
+   */
   doRefresh(refresher) {
     console.log('Begin async operation', refresher);
     this.refreshLoad(refresher);
@@ -66,14 +116,21 @@ export class HomePage {
       refresher.complete();
     }, 2000);
   }
-  loadPosts( page ) {
 
+  /**
+   * The loadPost() function takes a page number and fetches an api response for
+   * wordpress using that page number and returns a promise of that api call.
+   * Used in the loadMore() function which adds infinite scroll capability.
+   *
+   * @params page
+   *
+   * @returns {Promise<T>}
+   */
+  loadPosts( page ) {
     if( !page ) {
       let page = '1';
     }
-
     return new Promise(resolve => {
-
       this.http.get( this.url + '&page=' + page )
         .map(res => res.json())
         .subscribe(data => {
@@ -84,11 +141,18 @@ export class HomePage {
 
     });
   }
-  itemTapped(event, item) {
-    this.nav.push(PostDetailPage, {
-      item: item
-    });
-  }
+
+  /**
+   * The loadMore() function is the main infinite scroll handler. It increments the
+   * page number which is then passed to the loadPosts() function. That function then
+   * returns a promise. After the API call has bee completed, .then() loads the posts.
+   * The function then checks the number of posts loaded, if count >=1 the posts are
+   * pushed to the page, if not the infinite scroll ends.
+   *
+   * @param infiniteScroll
+   *
+   * @returns none
+   */
   loadMore(infiniteScroll) {
 
     this.page++;
