4c05d5f7e3c93f707152c0da3df43325d89502f1
Jared Brown <brownjt01@sunyjcc.edu>
2017-08-03 18:03:08 -0400

    Added significant documentation to the sermon.ts file

--- src/pages/sermon/sermon.ts
+++ src/pages/sermon/sermon.ts
@@ -8,10 +8,11 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { AlertController } from 'ionic-angular';
 
 /**
- * Generated class for the SermonPage page.
+ * Note on the Backend:
  *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
+ * As of the creation of this page (July 2017), the backend currently uses PHP to receive
+ * user data and send out emails with the questions and answers as well as
+ * a link to the Sermon Handout.
  */
 
 @Component({
@@ -33,10 +34,20 @@ export class SermonPage {
   msg: any;
   notes: any;
 
-  constructor(private alertCtrl: AlertController,public navCtrl: NavController, public navParams: NavParams, private http: Http, private nav: NavController,
-  private loading: LoadingController, private iab: InAppBrowser) {
+  constructor(private alertCtrl: AlertController,public navCtrl: NavController, public navParams: NavParams,
+              private http: Http, private nav: NavController, private loading: LoadingController,
+              private iab: InAppBrowser) {
   }
-
+  /**
+   * The ionViewDidLoad() function executes every time the page is loaded. This
+   * function does not, however, run every time the view is entered. This allows
+   * the page to be cached and prevents an api call if the user uses the back-button
+   * to navigate back to the front page from a linked article
+   *
+   * @params: none
+   *
+   * @returns: void
+   */
   ionViewDidLoad() {
     let loader = this.loading.create({content: 'Loading Questions...', showBackdrop: false});
     loader.present().then(() => {
@@ -48,9 +59,7 @@ export class SermonPage {
           this.emailFlag = true;
           this.handoutURL = this.handoutURL + this.items['acf']['url'];
           this.qArray = this.getQuestions();
-
-          //console.log(this.qArray);
-        }, error => {
+        }, () => {
           setTimeout( () => {
             loader.dismiss().then();
             this.nav.push(NetworkDownPage);
@@ -80,11 +89,11 @@ export class SermonPage {
   }
 
   validateEmail(email) {
+    //TODO: Split RE into multiple lines with RE Object
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   }
   submitHandler(){
-
     if (this.email != ""){
       this.removeBlanks();
       var emptyFlag = 0
@@ -121,37 +130,52 @@ export class SermonPage {
           this.finalSubmit();
         }
       }
-
-
-
     }else{
       let msg = "Whoops! You need to enter an your Email!";
       this.presentAlertSimple(msg, "Missing Email");
     }
   }
+
+  /**
+   * The finalSubmit() function is the function that submits all of the necessary information through
+   * an HTTP Request to the backend. The function combines all data into a single array (greatArray).
+   * The final submit function also waits for a response from the server to notify the user if the
+   * server was able to receive the info and send out an email successfully.
+   *
+   * @param none
+   *
+   *@returns void
+   */
   finalSubmit(){
     var greatArray = this.qArray.concat(this.sendArray);
     greatArray.unshift(this.email, this.handoutURL, this.notes);
-    console.log( greatArray );
-    console.log( JSON.stringify(greatArray));
+    //TODO: Remove domain literal
     this.http.post('http://www.app.busticog.org/' +
       'sermon.php', JSON.stringify(greatArray))
       .map(res => res.json())
       .subscribe(data => {
         this.response = data;
         if( this.response['pass'] != "yes"){
-          this.msg = "test"; //Not implemented in view
+          this.msg = "test"; //TODO: Fix this
           this.presentAlertSimple(this.msg, "Error");
         }else{
           this.presentAlertSimple(this.response['msg'], this.response['title']);
         }
       }, () => {
-        //If http request fails -
+        //If HTTP request fails -
         this.msg = "Network Error: Try again later and make sure " +
           "you are connected to the Internet.";
         this.presentAlertSimple(this.msg, "Error");
       });
   }
+
+  /**
+   * The presentAlertSimple() function creates an Ionic alert object with the provided
+   * parameters ($msg - The alert message, and $title - The title for the alert)
+   *
+   * @param $msg
+   * @param $title
+   */
   presentAlertSimple($msg, $title) {
     let alert = this.alertCtrl.create({
       title: $title,
@@ -160,6 +184,18 @@ export class SermonPage {
     });
     alert.present();
   }
+
+  /**
+   * The removeBlanks() function cuts down the length of the array for the answers to the questions.
+   * The largest amount of questions that can be provided in the backend is 10. Therefore, the valueArray is
+   * declared and instantiated with 10 empty strings. If less than 10 questions are provided, this function
+   * removes the blank index positions for the non-existent questions. A new array called the sendArray is
+   * created. The amount of answers transferred to the new array is determined by the number of questions
+   * asked (len of qArray).
+   *
+   * @param none
+   * @returns void
+   */
   removeBlanks(){
     for ( var i = 0; i < this.qArray.length; i++ ){
       this.sendArray[i] = this.valueArray[i];
